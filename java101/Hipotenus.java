95dbdb580ee1e8022c19e06c480730beb0ffa8d0
Fatih Yelekçi <fy3134@anadolusigorta.pvt>
2024-02-14 21:40:17 +0300

    hipotenus

--- java101/Hipotenus.java
+++ java101/Hipotenus.java
@@ -1,2 +1,18 @@
-package java101;public class Hipotenus {
+package java101;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Hipotenus {
+    public static void main(String[] args) {
+        double hipotenus, dikkenar1, dikkenar2;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("birinci dik kenarı giriniz: ");
+        dikkenar1 = input.nextDouble();
+        System.out.print("ikinci dik kenarı giriniz: ");
+        dikkenar2= input.nextDouble();
+        hipotenus = Math.sqrt((dikkenar1 * dikkenar1) + (dikkenar2 * dikkenar2));
+        System.out.println("hipotenüs: " + hipotenus);
+    }
 }
